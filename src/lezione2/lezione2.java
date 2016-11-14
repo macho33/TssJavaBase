d0838c563cd0f7e1295a1657420153a57dc4e275
tss <tss@debian-tss24>
2016-11-14 11:19:39 +0100

    seconda lezione

---
+++ src/lezione2/lezione2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione2;
+
+/**
+ *esempio di commenti in java
+ * @author tss
+ */
+public class lezione2 {
+
+    /**
+     * 
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //singola riga
+        
+        
+        /* ahjdhdhdlkhdl
+        jhkdldjldjdl
+        */
+        
+        
+        System.out.println("start esempio commenti..");
+        
+    }
+    
+}
