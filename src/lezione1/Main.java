5875783a8ab386007bcac6a7eacfba1bfc0414e8
tss <tss@debian-tss24>
2016-11-14 09:20:52 +0100

    prima commit

---
+++ src/lezione1/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezione1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("primo software ");
+        
+        
+        String frase = JOptionPane.showInputDialog("inserisci la frase da stampare");
+         System.out.println(frase );
+         
+        // TODO code application logic here
+    }
+    
+}
